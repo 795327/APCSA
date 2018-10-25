e6d997ee0bbc017a0934c019bbbc385a5dc5f683
795327 <795327@seq.org>
2018-10-25 10:32:14 -0700

    ArrayList Project start

---
+++ Projects/ArrayList Project 2/StudList.java
@@ -0,0 +1,33 @@
+
+/**
+ * StudList
+ *
+ * @author (Noel Salmeron)
+ * @version (1025)
+ */
+public class StudList
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class StudList
+     */
+    public StudList()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
