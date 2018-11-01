9b5c683707713763af65a646369463bb68a30f2a
795327 <795327@seq.org>
2018-10-31 22:56:01 -0700

    String Test 2 Review

---
+++ Labs/String Test 2 Review/Review.java
@@ -0,0 +1,24 @@
+
+/**
+ * Review
+ *
+ * @author (Noel Salmeron)
+ * @version (1031)
+ */
+public class Review{
+    public static void main(){
+        String str = "I bought a new computer!";
+        // 1 num makes substring start at the num
+        System.out.println(str.substring(10));
+        System.out.println(str.substring(0, 10));
+        System.out.println(str.substring(5, 20));
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        
+    }
+}
